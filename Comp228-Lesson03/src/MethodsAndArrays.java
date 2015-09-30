e76fd60618f21cf99e6a37f7fd2286b570923973
admin <admin@192.168.2.12>
2015-09-29 20:27:08 -0400

    added driver class to project

---
+++ Comp228-Lesson03/src/MethodsAndArrays.java
@@ -0,0 +1,12 @@
+
+public class MethodsAndArrays {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		long myLongVariable=1000000L;
+		int myIntVariable=(int)myLongVariable;
+		
+		System.out.println(myIntVariable);
+	}
+
+}
